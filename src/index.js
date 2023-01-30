c758307512762ee506ed69c1a79f7ae08fb2aa30
황나희 <nahhwang@gmarket.com>
2023-01-30 23:08:58 +0900

    구조세팅

---
+++ src/index.js
@@ -0,0 +1,32 @@
+/**
+ * @ param {string} content
+ * returns content를 두 번 출력해준다.
+ **/
+function Todo(아이디, 내용, 완료여부, 카테고리, 태그들) {
+  return 아이디;
+}
+
+function Create(내용) {
+  return 내용;
+}
+/**
+ * ID를 제외한 모든 속성을 수정할 수 있다.
+ * 특정 할 일의 특정 태그를 수정할 수 있다.
+ * @ param {string} content
+ * returns content를 두 번 출력해준다.
+ **/
+function Update(태그들) {}
+
+/**
+ * 특정 할 일의 모든 태그를 제거할 수 있다.
+ * 모든 할 일을 제거할 수 있다.
+ * 특정 할 일의 특정 태그를 삭제할 수 있다.
+ * 특정 할 일의 모든 태그를 제거할 수 있다.
+ * @param {*} 아이디
+ * @param {*} 내용
+ * @param {*} 태그들
+ * @returns
+ */
+function Delete(아이디, 내용, 태그들) {
+  return 내용;
+}
---
+++ 기초공부.js
@@ -0,0 +1,34 @@
+/**
+ * 이름, 나이 입력하면 합쳐서 문자로 뱉어주는 함수
+ * @param {string} name 사람이름 넣기
+ * @param {number} age 사람나이 넣기
+ * @returns 두 개 합쳐서 문자로 뱉어줌
+ */
+
+/**
+ * @version 1.3.0
+ * @see https://naver.com
+ */
+function hello(name, age) {
+  return name + age;
+}
+
+/**
+ * @readonly
+ * @const {number}
+ */
+
+/**
+ * @todo 할일 메모가능, 내일까지 뺄셈기능 추가
+ */
+
+/**
+ * @deprecated 이제 이 함수 그만쓰고 다른거 쓰라고 경고!
+ */
+
+hello("hee", 20);
+
+/**
+ * @type {string | number} 
+ * var name = 'kim';
+ */
