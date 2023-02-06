d3458baadaa084a3c99e3ce2cc6c3219c322120e
황나희 <nahhwang@gmarket.com>
2023-02-06 20:59:11 +0900

    param 추가

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 /**
- * @ param {string} content
- * returns content를 두 번 출력해준다.
+ * @param {string} id 아이디
+ * @param {string} content 내용
+ * @param {boolean} finished 완료여부
+ * @param {string} category 카테고리
+ * @param {Array} tags 태그들 
+ * returns 투두리스트
  **/
 function Todo(아이디, 내용, 완료여부, 카테고리, 태그들) {
   return 아이디;
@@ -22,11 +26,7 @@ function Update(태그들) {}
  * 모든 할 일을 제거할 수 있다.
  * 특정 할 일의 특정 태그를 삭제할 수 있다.
  * 특정 할 일의 모든 태그를 제거할 수 있다.
- * @param {*} 아이디
- * @param {*} 내용
- * @param {*} 태그들
- * @returns
- */
+ **/
 function Delete(아이디, 내용, 태그들) {
   return 내용;
 }
